2a96176915ed1ec5ac556e5f7f0716fb7ba8ae57
norbertmlynarski <bibiosm98@gmail.com>
2021-07-12 12:07:52 +0200

    Add Login and Registration Fragments

---
+++ app/src/main/java/com/example/internapp/authentication/LoginFragment.kt
@@ -0,0 +1,23 @@
+package com.example.internapp.authentication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.example.internapp.databinding.LoginFragmentBinding
+
+class LoginFragment : Fragment() {
+    private val viewModel: AuthenticationViewModel by activityViewModels()
+    private lateinit var binding: LoginFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = LoginFragmentBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}

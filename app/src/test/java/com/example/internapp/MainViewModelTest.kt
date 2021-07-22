09e01f191b2fafd54e35214baa56cd67bd3c295c
norbertmlynarski <bibiosm98@gmail.com>
2021-07-22 22:28:46 +0200

    Upgrade gradle version, add MainViewModelTest

---
+++ app/src/test/java/com/example/internapp/MainViewModelTest.kt
@@ -0,0 +1,13 @@
+package com.example.internapp
+
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class MainViewModelTest {
+    private lateinit var mainViewModel: MainViewModel
+    @Before
+    fun setUp() {
+        mainViewModel = MainViewModel()
+    }
+}
